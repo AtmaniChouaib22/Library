58f68713119bb0a05326ea2a87e937c5195d9304
Atmani Chouaib <atmanichouaib22@gmail.com>
2023-08-20 16:11:51 +0100

    i did all the logic and ui work

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+
 let booksLibrary = [];
 let titInp = document.querySelector('[data-title]'); 
 let authInp = document.querySelector('[data-author]'); 
@@ -6,15 +8,21 @@ let openModal = document.querySelector('[data-open]');
 let modal = document.querySelector("[data-dialog]"); 
 let cancel = document.querySelector("[data-cancel]"); 
 let addNew = document.querySelector("[data-add]"); 
-
+let libGrid = document.querySelector(".lib"); 
+//Book constructor
 function Book(title , author , pages){
     this.title = title ; 
     this.author = author ; 
     this.pages = pages ; 
 };
-
+//adding Function 
 function addBook(){
-    
+    //checking Null values
+    if(titInp.value ==='' || authInp.value==="" || pagInp.value===""){ 
+        window.alert("You didn't fill the input fields"); 
+        return ;
+    }
+
     let title = titInp.value ;
     let author = authInp.value ; 
     let pages = pagInp.value ; 
@@ -25,18 +33,35 @@ function addBook(){
     titInp.value = ""; 
     authInp.value = ""; 
     pagInp.value = ""; 
+    modal.close();
+    //creating the card
+    let cardBook = document.createElement("div"); 
+    let bookTitle = document.createElement("h4"); 
+    let bookAuthor = document.createElement("div"); 
+    let bookPages = document.createElement("div"); 
+
+    cardBook.classList.add("cardBox");
+    bookTitle.textContent = `title is:  ${title}` ; 
+    bookAuthor.textContent = `Author:  ${author}` ; 
+    bookPages.textContent = `${pages}  Pages`; 
+
+    cardBook.appendChild(bookTitle); 
+    cardBook.appendChild(bookAuthor);
+    cardBook.appendChild(bookPages);   
+    libGrid.appendChild(cardBook); 
 }; 
-
-cancel.addEventListener("click" , ()=> {
-    modal.close(); 
-})
-
+/*
 function showBooks(){
     booksLibrary.forEach((book)=>{
         console.table(book); 
     })
 };
+*/
 
+//event listeners
+cancel.addEventListener("click" , ()=> {
+    modal.close(); 
+})
 
 openModal.addEventListener("click", ()=>{
      modal.showModal(); 
@@ -44,10 +69,12 @@ openModal.addEventListener("click", ()=>{
 
 addNew.addEventListener("click" , (e) => {
     addBook(); 
-    showBooks(); 
     e.preventDefault();
-    modal.close();
 }); 
+
+});
+
+
 
 
 
